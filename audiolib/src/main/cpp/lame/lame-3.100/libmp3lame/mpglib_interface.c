a08f995fa6245ba2fbe3f21987d4b3db9cbdef02
hd <hdshare@vip.qq.com>
2025-09-04 11:35:16 +0800

    fix: lame build error

--- audiolib/src/main/cpp/lame/lame-3.100/libmp3lame/mpglib_interface.c
+++ audiolib/src/main/cpp/lame/lame-3.100/libmp3lame/mpglib_interface.c
@@ -28,6 +28,8 @@
 # include <config.h>
 #endif
 
+#define HAVE_MPGLIB
+
 #ifdef HAVE_MPGLIB
 #define hip_global_struct mpstr_tag 
 #include "lame.h"
--- audiolib/src/main/cpp/lame/lame-3.100/mpglib/mpg123.h
+++ audiolib/src/main/cpp/lame/lame-3.100/mpglib/mpg123.h
@@ -26,15 +26,15 @@
 #ifdef STDC_HEADERS
 # include <string.h>
 #else
-# ifndef HAVE_STRCHR
-#  define strchr index
-#  define strrchr rindex
-# endif
-char   *strchr(), *strrchr();
-# ifndef HAVE_MEMCPY
-#  define memcpy(d, s, n) bcopy ((s), (d), (n))
-#  define memmove(d, s, n) bcopy ((s), (d), (n))
-# endif
+//# ifndef HAVE_STRCHR
+//#  define strchr index
+//#  define strrchr rindex
+//# endif
+//char   *strchr(), *strrchr();
+//# ifndef HAVE_MEMCPY
+//#  define memcpy(d, s, n) bcopy ((s), (d), (n))
+//#  define memmove(d, s, n) bcopy ((s), (d), (n))
+//# endif
 #endif
 
 #include        <signal.h>
