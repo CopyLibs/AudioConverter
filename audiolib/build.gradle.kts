18267e61c2e405843781b3b03282dca8b6a89bf9
hd <hdshare@vip.qq.com>
2026-02-27 10:00:06 +0800

    chore: bump 1.0.0.beta2

--- audiolib/build.gradle.kts
+++ audiolib/build.gradle.kts
@@ -48,7 +48,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.copylibs",
         artifactId = "audio-converter",
-        version = "1.0.0.beta1"
+        version = "1.0.0.beta2"
     )
 
     pom {
