ef54cf408c2ac35d036effe76be54fca8a8f7e37
hd <hdshare@vip.qq.com>
2026-03-02 23:11:07 +0800

    perf: demo add convert pcm

--- app/src/main/java/io/github/copylibs/audiolib/MainActivity.kt
+++ app/src/main/java/io/github/copylibs/audiolib/MainActivity.kt
@@ -20,6 +20,16 @@ class MainActivity : Activity() {
     private fun initView() {
         binding.edtInputPath.setText(filesDir.absolutePath)
         binding.edtOutputPath.setText(filesDir.absolutePath)
+        binding.btnMp3ToPcm.setOnClickListener {
+            val mp3Path = binding.edtInputPath.text.toString()
+            val pcmPath = binding.edtOutputPath.text.toString()
+            AudioUtil.mp3ToPcm(File(mp3Path)).copyTo(File(pcmPath), true)
+        }
+        binding.btnPcmToMp3.setOnClickListener {
+            val pcmPath = binding.edtInputPath.text.toString()
+            val mp3Path = binding.edtOutputPath.text.toString()
+            AudioUtil.pcmToMp3(File(pcmPath)).copyTo(File(mp3Path), true)
+        }
         binding.btnMp3ToSilk.setOnClickListener {
             val mp3Path = binding.edtInputPath.text.toString()
             val silkPath = binding.edtOutputPath.text.toString()
